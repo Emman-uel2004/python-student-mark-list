794fff165f226fafc94d9aabef63a9aff6aab8c3
Emman-uel2004 <antonyemmanuel148@gmail.com>
2026-07-10 12:02:49 +0530

    Update student_marklist.py

--- student_marklist.py
+++ student_marklist.py
@@ -8,6 +8,19 @@ total=sub1+sub2+sub3+sub4+sub5
 average=total/5
 print(f"total mark:{total}")
 print(f"average mark:{average}")
+def total_grade(mark):
+  if total>=500:
+    return("invalid mark")
+  elif total>=450:
+    return("A grade")
+  elif total>=400:
+    return("B grade")
+  elif total>=350:
+    return("c grade")
+  elif total>=300:
+    return("D grade")
+  else:
+    return("fail")
 def grade(mark):
   if mark>=90:
     return("A grade")
@@ -19,6 +32,7 @@ def grade(mark):
     return("average")
   else:
     return("fail")
+print(f"total grade:{total_grade(total)")
 print(f"tamil grade:{grade(sub1)}")
 print(f"english grade:{grade(sub2)}")
 print(f"math grarde:{grade(sub3)}")
